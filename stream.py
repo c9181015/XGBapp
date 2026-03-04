dfcc054752e035d38f67c301beb086a615eceaa5
c9181015 <1561643667@qq.com>
2026-03-04 23:08:52 +0800

    Add files via upload

--- stream.py
+++ stream.py
@@ -46,7 +46,7 @@ text = {
         "high": "高风险",
         "low": "低风险",
         "threshold": "阈值",
-        "disclaimer": "免责声明：本结果仅供参考，不可作为诊断或治疗决策依据。"
+        "disclaimer": "免责声明：本结果仅供参考，不可作为诊断或治疗决策依据。",
         "feature_labels": {
             "Antibiotic use": "抗生素使用",
             "Circulatory Failure": "循环衰竭",
